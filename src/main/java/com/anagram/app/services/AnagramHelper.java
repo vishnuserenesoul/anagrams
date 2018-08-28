3ba374ab3e05f03463a3bdb7d0e8072af9a2724c
vishnuserenesoul <vishnuserenesoul@users.noreply.github.com>
2018-08-28 15:15:19 -0700

    Update AnagramHelper.java

--- src/main/java/com/anagram/app/services/AnagramHelper.java
+++ src/main/java/com/anagram/app/services/AnagramHelper.java
@@ -9,23 +9,10 @@ import java.util.stream.Collectors;
 
 public class AnagramHelper {
 
-
     public static Set<String> getAnagramsFromDictionary(String input)  {
 
         return StringPermutations.get(input).stream().filter( key -> !input.equals(key)&& Dictionary.getInstance().contains(key)).collect(Collectors.toCollection(TreeSet::new));
 
-
     }
-
-//    public static void main(String[] args) {
-//        for (Object s:
-//        Dictionary.getInstance().withInitializedData("https://users.cs.duke.edu/~ola/ap/linuxwords").keys()) {
-//            List<String> out = getAnagramsFromDictionary(String.valueOf(s));
-//            if(out!=null && out.size()>0)
-//            {
-//                System.out.println(out.toArray().toString());
-//            }
-//        }
-//    }
 
 }
